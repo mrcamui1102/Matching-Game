56d2267d94bb9817f1cac691cf8e0d8cc0872ce1
mrcamui1102 <1851050077lap@ou.edu.vn>
2021-06-09 15:53:28 +0700

    update maching game

---
+++ memorygame/memorygame/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace memorygame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbClick = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbDiem = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoatGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbTime);
+            this.groupBox1.Controls.Add(this.lbClick);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lbDiem);
+            this.groupBox1.Location = new System.Drawing.Point(634, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 209);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 31);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Click:";
+            // 
+            // lbTime
+            // 
+            this.lbTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTime.Location = new System.Drawing.Point(133, 151);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(80, 35);
+            this.lbTime.TabIndex = 0;
+            // 
+            // lbClick
+            // 
+            this.lbClick.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbClick.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbClick.Location = new System.Drawing.Point(133, 98);
+            this.lbClick.Name = "lbClick";
+            this.lbClick.Size = new System.Drawing.Size(80, 35);
+            this.lbClick.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Điểm số:";
+            // 
+            // lbDiem
+            // 
+            this.lbDiem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbDiem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbDiem.Location = new System.Drawing.Point(133, 50);
+            this.lbDiem.Name = "lbDiem";
+            this.lbDiem.Size = new System.Drawing.Size(80, 35);
+            this.lbDiem.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(361, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Game Lật Hình";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(896, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGame,
+            this.thoatGame});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGame
+            // 
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(132, 22);
+            this.newGame.Text = "New Game";
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // thoatGame
+            // 
+            this.thoatGame.Name = "thoatGame";
+            this.thoatGame.Size = new System.Drawing.Size(132, 22);
+            this.thoatGame.Text = "Thoát";
+            this.thoatGame.Click += new System.EventHandler(this.thoatGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(896, 566);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbDiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbClick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGame;
+        private System.Windows.Forms.ToolStripMenuItem thoatGame;
+    }
+}
+
---
+++ memorygame/memorygame/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memorygame
+{
+    public partial class Form1 : Form
+    {
+
+        string path = "";
+        Random r = new Random();
+        PictureBox[] mangAnh;
+        int n = 12, dem = 0;
+        private DateTime curTime;
+        int diem = 0;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            path = Application.StartupPath + @"\image\";
+            mangAnh = new PictureBox[n];
+            int k = 0, j = 1;
+            for (int i = 0; i < mangAnh.Length; i++)
+            {
+                mangAnh[i] = new PictureBox();
+                mangAnh[i].Size = new Size(120, 120);
+                mangAnh[i].Image = Image.FromFile(path + "0.png");
+                mangAnh[i].Location = new Point(30 + k * 130, 30 + j * 130);
+                mangAnh[i].Enabled = true;
+                mangAnh[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                mangAnh[i].Click += new EventHandler(pic_Click);
+                this.Controls.Add(mangAnh[i]);
+                k++;
+                if (k == 4)
+                {
+                    k = 0; j++;
+                }
+            }
+        }
+
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            pic.Image = Image.FromFile(path + pic.Tag.ToString() + ".png");
+            count++;
+            lbClick.Text = count.ToString();
+            pic.Enabled = false;
+            //pic.Text = " ";
+            checkpic();
+            if (dem == mangAnh.Length / 2)
+            {
+                dem = 0;
+                timer2.Stop();
+                //Check_HighScore();
+                if (MessageBox.Show("Bạn đã hoàn thành trò chơi!\nBạn có muốn chơi mới?", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    rePlay();
+            }
+        }
+
+        private void checkpic()
+        {
+            for (int i = 0; i < mangAnh.Length - 1; i++)
+            {
+                for (int j = i + 1; j < mangAnh.Length; j++)
+                {
+                    if (mangAnh[i].Enabled == false && mangAnh[j].Enabled == false)       // lỗi chỗ này nếu ko để true
+                    {
+                        if (mangAnh[i].Tag.ToString() == mangAnh[j].Tag.ToString())
+                        {
+                            System.Threading.Thread.Sleep(300);
+                            mangAnh[i].Visible = false;
+                            mangAnh[j].Visible = false;
+
+                            mangAnh[i].Enabled = true;     // vẫn để true lại là để tránh lỗi khi click cặp hình sau
+                            mangAnh[j].Enabled = true;     // nếu để false thì sẽ bị lỗi khi duyệt đều kiện ở vòng lặp trên
+                            dem++;
+                            diem += 20;
+                        }
+                        else
+                        {
+                            System.Threading.Thread.Sleep(300);
+                            mangAnh[i].Enabled = true;
+                            mangAnh[j].Enabled = true;
+                            mangAnh[i].Image = Image.FromFile(path + "0.png");
+                            mangAnh[j].Image = Image.FromFile(path + "0.png");
+                            TimeSpan dt = new TimeSpan(0, 0, 0, 1, 0);
+                            curTime = curTime.Subtract(dt);
+                            if (diem > 0)
+                                diem -= 5;
+                             
+                        }
+                        lbDiem.Text = diem.ToString();
+                       
+                    }
+                }
+            }
+        }
+
+
+        private void ranPic()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < mangAnh.Length / 2; i++)
+            {
+                int j = r.Next(1, 16);
+                mangAnh[i].Tag = j;
+                list.Add(j);
+            }
+            for (int i = mangAnh.Length / 2; i < mangAnh.Length; i++)
+            {
+                int x = r.Next(0, list.Count - 1);       // lấy ngẫy nhiên vị trí 1 phần từ trong list
+                mangAnh[i].Tag = list[x];
+                list.RemoveAt(x);
+            }
+
+        }
+
+        private void GameStart()
+        {
+            curTime = new DateTime(2020, 1, 1, 0, 0, 30);
+            Init();
+            diem = 0;
+            dem = 0;
+            count = 0;
+            lbDiem.Text = "";
+            lbClick.Text = "";
+            timer2.Start();
+            ranPic();
+            //Show_HighScore();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GameStart();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn đóng ứng dụng?", "Cảnh báo",
+                                MessageBoxButtons.YesNo) == DialogResult.No)
+                e.Cancel = true;
+        }
+        int dx = 10;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label2.Left < 0 || label2.Right > ClientRectangle.Width)
+                dx = -dx;
+            label2.Left += dx;
+
+                
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            TimeSpan dt = new TimeSpan(0, 0, 0, 1, 0);
+
+            
+            curTime = curTime.Subtract(dt);
+            lbTime.Text = curTime.Second.ToString();
+
+            if (curTime.Second == 0)
+            {
+                timer2.Enabled = false;
+                if (MessageBox.Show("Bạn đã thua!\nBạn muốn chơi lại?", "Thông báo", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    rePlay();
+                else
+                {
+                    for (int i = 0; i < mangAnh.Length; i++)
+                    {
+                        mangAnh[i].Visible = false;
+                    }
+                }    
+
+            }
+        }
+
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            rePlay();
+        }
+
+        private void thoatGame_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void rePlay()
+        {
+            // xóa các arrPic đã được tạo bằng cách ẩn nó đi                        // chưa giải phóng được bộ nhớ
+            for (int i = 0; i < mangAnh.Length; i++)
+            {
+                mangAnh[i].Visible = false;
+            }
+
+
+            //pgbTimePlay.Maximum = 0;
+            GameStart();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
